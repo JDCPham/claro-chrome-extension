b515a3acd0d7e6840f25ba872f5ae2c2dbc8a00f
--replace-all <jdcpham@outlook.com>
2020-11-15 11:43:50 +0000

    Add Bootstrap and Options

--- app/options/options.js
+++ app/options/options.js
@@ -1,15 +1,73 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
+var yearly_gross = 45000;
+var api_key = 3128649984;
+let annualSalaryInput = document.getElementById("annual-salary");
+let dailyHoursInput = document.getElementById("daily-hours");
+let updateButton = document.getElementById("update-button");
+
+setInputFromStorage();
+
+updateButton.addEventListener('click', () => {
+  $.get(`https://www.income-tax.co.uk/api/${api_key}/${annualSalaryInput.value}/`, function (data) {
+            // data is the returned results, do whatever you want with it
+            console.log(data);
+
+            let salary = {
+              annual: {
+                gross: annualSalaryInput.value,
+                net: data['take_home']['yearly']
+              },
+              daily: {
+                gross: data['income']['daily'],
+                net: data['take_home']['daily']
+              }
+            }
+
+            let hours = {
+              daily: dailyHoursInput.value
+            }
+            setStorage(salary, hours)
+
+        })
+        .done(function () {
+            setInputFromStorage();
+            alert("success")
+        })
+        .fail(function () {
+            //alert( "error" );
+        })
+        .always(function () {
+            //alert( "finished" );
+        });
+			
+})
+
+function setStorage(salary, hours) {
+  chrome.storage.sync.set({
+    salary: salary,
+    hours: hours
+  })
 }
-constructOptions(kButtonColors);
+
+function setInputFromStorage() {
+  chrome.storage.sync.get(['salary', 'hours'], (data) => {
+    annualSalaryInput.value = data['salary']['annual']['gross']
+    dailyHoursInput.value = data['hours']['daily']
+  })
+}
+
+
+chrome.storage.sync.set({
+  salary: {
+    annual: {
+      gross: 35000.00,
+      net: 27440.00
+    },
+    daily: {
+      gross: 134.62,
+      net: 105.54
+    }
+  },
+  hours: {
+    daily: 8
+  }
+})
--- app/popup/index.js
+++ app/popup/index.js
@@ -1,15 +1,11 @@
-// Get background page.
-var bkg = chrome.extension.getBackgroundPage();
-
 // Get options button.
 var optionsButton = document.getElementById("options-button");
 
 // Set event listener.
 optionsButton.addEventListener("click", () => {
-    bkg.console.log("HELLO")
-    window.open("chrome-extension://jiklkpjoiaganoomocngfdafbjmcomne/app/options/options.html")
+    openOptions();
 })
 
-function doSomething() {
-    alert("hi")
+function openOptions() {
+    window.open("chrome-extension://jiklkpjoiaganoomocngfdafbjmcomne/app/options/options.html")
 }
